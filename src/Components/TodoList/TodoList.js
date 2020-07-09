5056cd9384e151a2bec48a23ea175b310939602c
Mateusz <mateusz@o2.pl>
2020-07-09 09:36:50 +0200

    Add validation

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -5,12 +5,21 @@ import Todo from './Todo';
 function   TodoList(props){
     const [todoList, setTodoList] = useState([]);
     const [inputValue, setInputValue] = useState("");
+    const [error, setError] = useState("");
     
     const handleInputChange = (event) => {
         const { value, name } = event.target;
         setInputValue(value);
     }
     const handleButtonClick = (event) => {
+
+        if(todoList.some(todo => todo === inputValue)){
+            setError("Taka wartość już istnieje.");
+            return;
+        }else{
+            setError("");
+        }
+
         setTodoList([...todoList, inputValue]);
         setInputValue("");
     }
@@ -18,17 +27,24 @@ function   TodoList(props){
     return (
         <div>
             <h1>Moja aplikacja Todo</h1>
+
             <input 
                 name="TodoInput"
                 placeholder="Co bedziemy dzisiaj robic?"
                 value={inputValue}
                 onChange={handleInputChange}
             />
+
             <button
                 onClick={handleButtonClick}
             >
                 dodaj
             </button>
+
+            {error &&
+                <p>{error}</p>
+            }
+
             {todoList.map((todo)=> (
                 <Todo 
                     key={todo}
