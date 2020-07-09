db8de0e54d75e4f2d836032c478b80ec2d1c72e6
Mateusz <mateusz@o2.pl>
2020-07-09 08:53:01 +0200

    Todo elements

--- src/Components/TodoList/Todo.js
+++ src/Components/TodoList/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Todo(props){
     return(
         <div>
-            Moje zadanie
+            {props.todo}
         </div>
     )
 }
--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Todo from './Todo';
 
 function   TodoList(props){
+    const [todoList, setTodoList] = useState(["1","2"]);
     return (
         <div>
-            Moja aplikacja Todo
+            Moja aplikacja Todo:
+            {todoList.map((todo)=> (
+                <Todo 
+                    todo={todo}
+                />
+            ))}
         </div>
     )
 }
