71e372143a86e46604947723fa6845ed3031da23
Mateusz <mateusz@o2.pl>
2020-07-09 09:00:41 +0200

    Added controllable input

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -3,12 +3,26 @@ import React, { useState } from 'react'
 import Todo from './Todo';
 
 function   TodoList(props){
-    const [todoList, setTodoList] = useState(["1","2"]);
+    const [todoList, setTodoList] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    
+    const handleInputChange = (event) => {
+        const { value, name } = event.target;
+        setInputValue(value);
+    }
+
     return (
         <div>
-            Moja aplikacja Todo:
+            Moja aplikacja Todo
+            <input 
+                name="TodoInput"
+                placeholder="Co bedziemy dzisiaj robic?"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
             {todoList.map((todo)=> (
                 <Todo 
+                    key={todo}
                     todo={todo}
                 />
             ))}
